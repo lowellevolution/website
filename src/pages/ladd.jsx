74804c174498f68bad9793c8ca545a449c5b14c3
Peter Martin <peter.martin@airbnb.com>
2019-03-01 09:00:21 -0500

    fix huge image problem

--- src/pages/ladd.jsx
+++ src/pages/ladd.jsx
@@ -7,15 +7,15 @@ import Layout from '../templates/layout';
 import SEO from '../components/seo';
 
 const Ladd = ({data}) => {
-  const rightImage = data.rightImage.childImageSharp.original.src;
-  const leftImage = data.leftImage.childImageSharp.original.src;
+  const rightImage = data.rightImage.childImageSharp.fixed.src;
+  const leftImage = data.leftImage.childImageSharp.fixed.src;
 
   console.log(rightImage);
   return (
     <>
     <Layout>
       <SEO title="testing" description="testing" />
-      <div className="container">
+      <div className="">
         <ReactCompareImage leftImage={leftImage} rightImage={rightImage} />;
         {/* <ReactCompareImage leftImage="/images/current.png" rightImage="/images/new.png" /> */}
       </div>
@@ -28,13 +28,19 @@ export const query = graphql`
   query {
     rightImage: file(relativePath: { eq: "new.png" }) {
     	childImageSharp {
+        fixed(width: 2000) {
+          src
+        }
         original {
           src
         }
     	}
     }
-    leftImage: file(relativePath: { eq: "old.png" }) {
+    leftImage: file(relativePath: { eq: "old.jpg" }) {
     	childImageSharp {
+        fixed(width: 2000) {
+          src
+        }
         original {
           src
         }
